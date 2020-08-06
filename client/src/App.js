56d919686ecd535e5fa2090db209682668e39fb0
ehbong <hangillw86@gmail.com>
2020-08-06 23:41:16 +0900

    Add get Customers Rest Api Module

--- client/src/App.js
+++ client/src/App.js
@@ -22,33 +22,24 @@ const styles = theme => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: 'https://placeimg.com/64/64/1',
-    name: '홍길동',
-    birthday: '961222',
-    gender: '남자',
-    job: '대학생',
-  },
-  {
-    id: 2,
-    image: 'https://placeimg.com/64/64/2',
-    name: '김철수',
-    birthday: '931222',
-    gender: '남자',
-    job: '군인',
-  },
-  {
-    id: 3,
-    image: 'https://placeimg.com/64/64/3',
-    name: '손흥민',
-    birthday: '921222',
-    gender: '남자',
-    job: '축구선수',
-  },
-];
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async ()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     console.log(this.props);
@@ -68,19 +59,18 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-                      customers.map(c => ( // 맵 함수를 사용할때는 key 가 있어야 한다.
-                        <Customer
-                          key={c.id}
-                          id={c.id}
-                          image={c.image}
-                          name={c.name}
-                          birthday={c.birthday}
-                          gender={c.gender}
-                          job={c.job}
-                        />
-                      ))
-            }
-
+              this.state.customers ? this.state.customers.map(c => ( // 맵 함수를 사용할때는 key 가 있어야 한다.
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              ))
+            : ""}
           </TableBody>
         </Table>
       </Paper>
